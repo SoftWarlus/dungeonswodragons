ac61dc1ea4d4e116d1f7aab0c31e8fbe73893478
SoftWarlus <oreobank@gmail.com>
2020-04-24 09:31:24 -0400

    Minor Updates

---
+++ Senior Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    private Vector3 targetPos;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        targetPos = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, speed * Time.deltaTime);
+    }
+}
